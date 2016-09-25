a2535f42b41e7dfddf325ac3d33acff86b274969
birdbrother <birdbrother@gmail.com>
2016-09-25 16:17:10 +0800

    page index.new

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,6 +1,22 @@
 class NovelsController < ApplicationController
   def index
-
+    @novels = Novel.all
   end
+  def new
+    @novel = Novel.new
+  end
+  def create
+    @novel = Novel.new(param_novel)
+    @novel.save
+    redirect_to novels_path
+  end
+
+
+
+  private
+  def param_novel
+    params.require(:novel).permit(:name, :genre, :description)
+  end
+
 end
 
--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,2 +1,3 @@
 class Novel < ApplicationRecord
+  validates_presence_of :name
 end
